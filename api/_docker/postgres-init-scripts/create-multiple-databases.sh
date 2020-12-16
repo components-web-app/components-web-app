c093a3f688a880ddfbb001406d6ca419beace3d5
Daniel West <daniel@silverback.is>
2020-12-16 13:38:01 +0000

    Fix pgsql init create extensions on created databases

--- api/_docker/postgres-init-scripts/create-multiple-databases.sh
+++ api/_docker/postgres-init-scripts/create-multiple-databases.sh
@@ -6,13 +6,16 @@ set -u
 function create_user_and_database() {
 	local database=$1
 	echo "  Creating user and database '$database'"
-	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
+	psql -v ON_ERROR_STOP=1 --username="$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $database;
 	    CREATE DATABASE $database;
 	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
-	    CREATE EXTENSION IF NOT EXISTS citext;
-	    CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 EOSQL
+	psql -v ON_ERROR_STOP=1 --username="$POSTGRES_USER" --dbname="$database" <<-EOSQL
+			CREATE EXTENSION IF NOT EXISTS citext SCHEMA public;
+			CREATE EXTENSION IF NOT EXISTS "uuid-ossp" SCHEMA public;
+			SELECT * FROM pg_extension;
+	EOSQL
 }
 
 if [ -n "$POSTGRES_MULTIPLE_DATABASES" ]; then
