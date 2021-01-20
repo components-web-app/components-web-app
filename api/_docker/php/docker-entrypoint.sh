c1ec30a6956bd10526e8e100622830b1ce29aeb0
Daniel West <daniel@silverback.is>
2021-01-20 17:22:48 +0000

    Optimisations

--- api/_docker/php/docker-entrypoint.sh
+++ api/_docker/php/docker-entrypoint.sh
@@ -31,13 +31,26 @@ if [ "$1" = 'php-fpm' ] || [ "$1" = 'php' ] || [ "$1" = 'bin/console' ]; then
 		composer install --prefer-dist --no-progress --no-suggest --no-interaction
 	fi
 
-	echo "Waiting for db to be ready..."
-	until bin/console doctrine:query:sql "SELECT 1" > /dev/null 2>&1; do
-		sleep 1
-	done
+	if grep -q DATABASE_URL= .env; then
+		echo "Waiting for database to be ready..."
+		ATTEMPTS_LEFT_TO_REACH_DATABASE=60
+		until [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ] || DATABASE_ERROR=$(php bin/console doctrine:query:sql -q "SELECT 1" 2>&1); do
+			sleep 1
+			ATTEMPTS_LEFT_TO_REACH_DATABASE=$((ATTEMPTS_LEFT_TO_REACH_DATABASE - 1))
+			echo "Still waiting for database to be ready... Or maybe the database is not reachable. $ATTEMPTS_LEFT_TO_REACH_DATABASE attempts left."
+		done
 
-	if ls -A src/Migrations/*.php > /dev/null 2>&1; then
-		bin/console doctrine:migrations:migrate --no-interaction
+		if [ $ATTEMPTS_LEFT_TO_REACH_DATABASE -eq 0 ]; then
+			echo "The database is not up or not reachable:"
+			echo "$DATABASE_ERROR"
+			exit 1
+		else
+			echo "The database is now ready and reachable"
+		fi
+
+		if ls -A migrations/*.php >/dev/null 2>&1; then
+			php bin/console doctrine:migrations:migrate --no-interaction
+		fi
 	fi
 fi
 
