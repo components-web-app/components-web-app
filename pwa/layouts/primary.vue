1f198c9e29e41b7ff4b43adb565d5a473ff4f0c7
Daniel West <daniel@silverback.is>
2021-03-27 12:27:17 +0000

    Include components manager in layout

--- pwa/layouts/primary.vue
+++ pwa/layouts/primary.vue
@@ -27,15 +27,17 @@
         v-bind="componentCollectionProps"
       />
     </div>
+    <cwa-component-manager />
   </div>
 </template>
 
 <script>
 import LayoutMixin from '@cwa/nuxt-module/core/mixins/LayoutMixin'
 import CwaRefreshBar from '@cwa/nuxt-module/core/templates/components/admin/cwa-refresh-bar.vue'
+import CwaComponentManager from '@cwa/nuxt-module/core/templates/components/admin/cwa-component-manager.vue'
 
 export default {
-  components: { CwaRefreshBar },
+  components: { CwaComponentManager, CwaRefreshBar },
   mixins: [LayoutMixin]
 }
 </script>
