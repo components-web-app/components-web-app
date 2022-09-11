fa7a4eaf52f00cc13e6de3ebdb205e3387683071
silverbackdan <daniel@silverback.is>
2022-09-11 16:34:21 +0100

    Update ACB for form event listener auto register

--- api/src/Entity/BlogArticleData.php
+++ api/src/Entity/BlogArticleData.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\Entity;
 
-
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
 use ApiPlatform\Doctrine\Orm\Filter\SearchFilter;
 use ApiPlatform\Metadata\ApiFilter;
