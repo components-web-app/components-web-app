52364d11c3989809ac46d4f69c3d818bd80bc935
silverbackdan <daniel@silverback.is>
2022-11-16 17:33:12 +0000

    Create new migrations file to init

--- api/migrations/Version20221116173257.php
+++ api/migrations/Version20221116173257.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220912193355 extends AbstractMigration
+final class Version20221116173257 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,19 +20,8 @@ final class Version20220912193355 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE _acb_abstract_component (id UUID NOT NULL, hero_id UUID DEFAULT NULL, html_content_id UUID DEFAULT NULL, image_id UUID DEFAULT NULL, route_id UUID DEFAULT NULL, navigation_link_id UUID DEFAULT NULL, ui_component VARCHAR(255) DEFAULT NULL, ui_class_names JSON DEFAULT NULL, dtype VARCHAR(255) NOT NULL, html TEXT DEFAULT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, filename VARCHAR(255) DEFAULT NULL, label TEXT DEFAULT NULL, resource_class VARCHAR(255) DEFAULT NULL, per_page INT DEFAULT NULL, default_query_parameters JSON DEFAULT NULL, form_type VARCHAR(255) DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, modified_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE INDEX IDX_4EBC1A1945B0BCD ON _acb_abstract_component (hero_id)');
-        $this->addSql('CREATE INDEX IDX_4EBC1A19414CCF0D ON _acb_abstract_component (html_content_id)');
-        $this->addSql('CREATE INDEX IDX_4EBC1A193DA5256D ON _acb_abstract_component (image_id)');
-        $this->addSql('CREATE INDEX IDX_4EBC1A1934ECB4E6 ON _acb_abstract_component (route_id)');
-        $this->addSql('CREATE INDEX IDX_4EBC1A19EB260C02 ON _acb_abstract_component (navigation_link_id)');
+        $this->addSql('CREATE TABLE _acb_abstract_component (id UUID NOT NULL, ui_component VARCHAR(255) DEFAULT NULL, ui_class_names JSON DEFAULT NULL, dtype VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN _acb_abstract_component.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN _acb_abstract_component.hero_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN _acb_abstract_component.html_content_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN _acb_abstract_component.image_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN _acb_abstract_component.route_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN _acb_abstract_component.navigation_link_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('COMMENT ON COLUMN _acb_abstract_component.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE _acb_abstract_component_component_group (abstract_component_id UUID NOT NULL, component_group_id UUID NOT NULL, PRIMARY KEY(abstract_component_id, component_group_id))');
         $this->addSql('CREATE INDEX IDX_8217637E71C016CE ON _acb_abstract_component_component_group (abstract_component_id)');
         $this->addSql('CREATE INDEX IDX_8217637EBC564DAA ON _acb_abstract_component_component_group (component_group_id)');
@@ -47,6 +36,8 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN _acb_abstract_page_data.parent_route_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN _acb_abstract_page_data.page_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN _acb_abstract_page_data.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE _acb_collection (id UUID NOT NULL, resource_class VARCHAR(255) DEFAULT NULL, per_page INT DEFAULT NULL, default_query_parameters JSON DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN _acb_collection.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE _acb_component_group (id UUID NOT NULL, reference VARCHAR(255) NOT NULL, location VARCHAR(255) NOT NULL, allowed_components TEXT DEFAULT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, modified_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_83287552AEA34913 ON _acb_component_group (reference)');
         $this->addSql('COMMENT ON COLUMN _acb_component_group.id IS \'(DC2Type:uuid)\'');
@@ -59,6 +50,9 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN _acb_component_position.component_group_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN _acb_component_position.component_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN _acb_component_position.created_at IS \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('CREATE TABLE _acb_form (id UUID NOT NULL, form_type VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, modified_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('COMMENT ON COLUMN _acb_form.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN _acb_form.created_at IS \'(DC2Type:datetime_immutable)\'');
         $this->addSql('CREATE TABLE _acb_imagine_cached_file_metadata (id UUID NOT NULL, path VARCHAR(255) NOT NULL, mime_type VARCHAR(255) NOT NULL, file_size INT NOT NULL, width INT DEFAULT NULL, height INT DEFAULT NULL, filter VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX unique_cache_item ON _acb_imagine_cached_file_metadata (path, filter)');
         $this->addSql('COMMENT ON COLUMN _acb_imagine_cached_file_metadata.id IS \'(DC2Type:uuid)\'');
@@ -96,6 +90,20 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('CREATE INDEX IDX_476D9BE5414CCF0D ON blog_article_data (html_content_id)');
         $this->addSql('COMMENT ON COLUMN blog_article_data.id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN blog_article_data.html_content_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE html_content (id UUID NOT NULL, html_content_id UUID DEFAULT NULL, html TEXT DEFAULT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_6A910E25414CCF0D ON html_content (html_content_id)');
+        $this->addSql('COMMENT ON COLUMN html_content.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN html_content.html_content_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE image (id UUID NOT NULL, image_id UUID DEFAULT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, filename VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_C53D045F3DA5256D ON image (image_id)');
+        $this->addSql('COMMENT ON COLUMN image.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN image.image_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('CREATE TABLE navigation_link (id UUID NOT NULL, route_id UUID DEFAULT NULL, navigation_link_id UUID DEFAULT NULL, label TEXT DEFAULT NULL, published_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE INDEX IDX_12C4C8334ECB4E6 ON navigation_link (route_id)');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_12C4C83EB260C02 ON navigation_link (navigation_link_id)');
+        $this->addSql('COMMENT ON COLUMN navigation_link.id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN navigation_link.route_id IS \'(DC2Type:uuid)\'');
+        $this->addSql('COMMENT ON COLUMN navigation_link.navigation_link_id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE refresh_token (id UUID NOT NULL, user_id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expired_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, version INT DEFAULT 1 NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_C74F2195A76ED395 ON refresh_token (user_id)');
         $this->addSql('COMMENT ON COLUMN refresh_token.id IS \'(DC2Type:uuid)\'');
@@ -106,18 +114,15 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN "user".id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN "user".roles IS \'(DC2Type:array)\'');
         $this->addSql('COMMENT ON COLUMN "user".created_at IS \'(DC2Type:datetime_immutable)\'');
-        $this->addSql('ALTER TABLE _acb_abstract_component ADD CONSTRAINT FK_4EBC1A1945B0BCD FOREIGN KEY (hero_id) REFERENCES _acb_abstract_component (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE _acb_abstract_component ADD CONSTRAINT FK_4EBC1A19414CCF0D FOREIGN KEY (html_content_id) REFERENCES _acb_abstract_component (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE _acb_abstract_component ADD CONSTRAINT FK_4EBC1A193DA5256D FOREIGN KEY (image_id) REFERENCES _acb_abstract_component (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE _acb_abstract_component ADD CONSTRAINT FK_4EBC1A1934ECB4E6 FOREIGN KEY (route_id) REFERENCES _acb_route (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE _acb_abstract_component ADD CONSTRAINT FK_4EBC1A19EB260C02 FOREIGN KEY (navigation_link_id) REFERENCES _acb_abstract_component (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_abstract_component_component_group ADD CONSTRAINT FK_8217637E71C016CE FOREIGN KEY (abstract_component_id) REFERENCES _acb_abstract_component (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_abstract_component_component_group ADD CONSTRAINT FK_8217637EBC564DAA FOREIGN KEY (component_group_id) REFERENCES _acb_component_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_abstract_page_data ADD CONSTRAINT FK_2D7554F734ECB4E6 FOREIGN KEY (route_id) REFERENCES _acb_route (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_abstract_page_data ADD CONSTRAINT FK_2D7554F7AB211837 FOREIGN KEY (parent_route_id) REFERENCES _acb_route (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_abstract_page_data ADD CONSTRAINT FK_2D7554F7C4663E4 FOREIGN KEY (page_id) REFERENCES _acb_page (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE _acb_collection ADD CONSTRAINT FK_9FC12AB6BF396750 FOREIGN KEY (id) REFERENCES _acb_abstract_component (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_component_position ADD CONSTRAINT FK_D6FC2796BC564DAA FOREIGN KEY (component_group_id) REFERENCES _acb_component_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_component_position ADD CONSTRAINT FK_D6FC2796E2ABAFFF FOREIGN KEY (component_id) REFERENCES _acb_abstract_component (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE _acb_form ADD CONSTRAINT FK_614EB84CBF396750 FOREIGN KEY (id) REFERENCES _acb_abstract_component (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_layout_component_group ADD CONSTRAINT FK_5AAA48748C22AA1A FOREIGN KEY (layout_id) REFERENCES _acb_layout (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_layout_component_group ADD CONSTRAINT FK_5AAA4874BC564DAA FOREIGN KEY (component_group_id) REFERENCES _acb_component_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_page ADD CONSTRAINT FK_27CCF323AB211837 FOREIGN KEY (parent_route_id) REFERENCES _acb_route (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
@@ -126,26 +131,30 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('ALTER TABLE _acb_page_component_group ADD CONSTRAINT FK_9E7E5D75C4663E4 FOREIGN KEY (page_id) REFERENCES _acb_page (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_page_component_group ADD CONSTRAINT FK_9E7E5D75BC564DAA FOREIGN KEY (component_group_id) REFERENCES _acb_component_group (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE _acb_route ADD CONSTRAINT FK_9BFEB786C30C9E2B FOREIGN KEY (redirect) REFERENCES _acb_route (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE blog_article_data ADD CONSTRAINT FK_476D9BE5414CCF0D FOREIGN KEY (html_content_id) REFERENCES _acb_abstract_component (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE blog_article_data ADD CONSTRAINT FK_476D9BE5414CCF0D FOREIGN KEY (html_content_id) REFERENCES html_content (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE blog_article_data ADD CONSTRAINT FK_476D9BE5BF396750 FOREIGN KEY (id) REFERENCES _acb_abstract_page_data (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE html_content ADD CONSTRAINT FK_6A910E25414CCF0D FOREIGN KEY (html_content_id) REFERENCES html_content (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE html_content ADD CONSTRAINT FK_6A910E25BF396750 FOREIGN KEY (id) REFERENCES _acb_abstract_component (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045F3DA5256D FOREIGN KEY (image_id) REFERENCES image (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE image ADD CONSTRAINT FK_C53D045FBF396750 FOREIGN KEY (id) REFERENCES _acb_abstract_component (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE navigation_link ADD CONSTRAINT FK_12C4C8334ECB4E6 FOREIGN KEY (route_id) REFERENCES _acb_route (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE navigation_link ADD CONSTRAINT FK_12C4C83EB260C02 FOREIGN KEY (navigation_link_id) REFERENCES navigation_link (id) ON DELETE SET NULL NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE navigation_link ADD CONSTRAINT FK_12C4C83BF396750 FOREIGN KEY (id) REFERENCES _acb_abstract_component (id) ON DELETE CASCADE NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE refresh_token ADD CONSTRAINT FK_C74F2195A76ED395 FOREIGN KEY (user_id) REFERENCES "user" (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE _acb_abstract_component DROP CONSTRAINT FK_4EBC1A1945B0BCD');
-        $this->addSql('ALTER TABLE _acb_abstract_component DROP CONSTRAINT FK_4EBC1A19414CCF0D');
-        $this->addSql('ALTER TABLE _acb_abstract_component DROP CONSTRAINT FK_4EBC1A193DA5256D');
-        $this->addSql('ALTER TABLE _acb_abstract_component DROP CONSTRAINT FK_4EBC1A1934ECB4E6');
-        $this->addSql('ALTER TABLE _acb_abstract_component DROP CONSTRAINT FK_4EBC1A19EB260C02');
         $this->addSql('ALTER TABLE _acb_abstract_component_component_group DROP CONSTRAINT FK_8217637E71C016CE');
         $this->addSql('ALTER TABLE _acb_abstract_component_component_group DROP CONSTRAINT FK_8217637EBC564DAA');
         $this->addSql('ALTER TABLE _acb_abstract_page_data DROP CONSTRAINT FK_2D7554F734ECB4E6');
         $this->addSql('ALTER TABLE _acb_abstract_page_data DROP CONSTRAINT FK_2D7554F7AB211837');
         $this->addSql('ALTER TABLE _acb_abstract_page_data DROP CONSTRAINT FK_2D7554F7C4663E4');
+        $this->addSql('ALTER TABLE _acb_collection DROP CONSTRAINT FK_9FC12AB6BF396750');
         $this->addSql('ALTER TABLE _acb_component_position DROP CONSTRAINT FK_D6FC2796BC564DAA');
         $this->addSql('ALTER TABLE _acb_component_position DROP CONSTRAINT FK_D6FC2796E2ABAFFF');
+        $this->addSql('ALTER TABLE _acb_form DROP CONSTRAINT FK_614EB84CBF396750');
         $this->addSql('ALTER TABLE _acb_layout_component_group DROP CONSTRAINT FK_5AAA48748C22AA1A');
         $this->addSql('ALTER TABLE _acb_layout_component_group DROP CONSTRAINT FK_5AAA4874BC564DAA');
         $this->addSql('ALTER TABLE _acb_page DROP CONSTRAINT FK_27CCF323AB211837');
@@ -156,12 +165,21 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('ALTER TABLE _acb_route DROP CONSTRAINT FK_9BFEB786C30C9E2B');
         $this->addSql('ALTER TABLE blog_article_data DROP CONSTRAINT FK_476D9BE5414CCF0D');
         $this->addSql('ALTER TABLE blog_article_data DROP CONSTRAINT FK_476D9BE5BF396750');
+        $this->addSql('ALTER TABLE html_content DROP CONSTRAINT FK_6A910E25414CCF0D');
+        $this->addSql('ALTER TABLE html_content DROP CONSTRAINT FK_6A910E25BF396750');
+        $this->addSql('ALTER TABLE image DROP CONSTRAINT FK_C53D045F3DA5256D');
+        $this->addSql('ALTER TABLE image DROP CONSTRAINT FK_C53D045FBF396750');
+        $this->addSql('ALTER TABLE navigation_link DROP CONSTRAINT FK_12C4C8334ECB4E6');
+        $this->addSql('ALTER TABLE navigation_link DROP CONSTRAINT FK_12C4C83EB260C02');
+        $this->addSql('ALTER TABLE navigation_link DROP CONSTRAINT FK_12C4C83BF396750');
         $this->addSql('ALTER TABLE refresh_token DROP CONSTRAINT FK_C74F2195A76ED395');
         $this->addSql('DROP TABLE _acb_abstract_component');
         $this->addSql('DROP TABLE _acb_abstract_component_component_group');
         $this->addSql('DROP TABLE _acb_abstract_page_data');
+        $this->addSql('DROP TABLE _acb_collection');
         $this->addSql('DROP TABLE _acb_component_group');
         $this->addSql('DROP TABLE _acb_component_position');
+        $this->addSql('DROP TABLE _acb_form');
         $this->addSql('DROP TABLE _acb_imagine_cached_file_metadata');
         $this->addSql('DROP TABLE _acb_layout');
         $this->addSql('DROP TABLE _acb_layout_component_group');
@@ -169,6 +187,9 @@ final class Version20220912193355 extends AbstractMigration
         $this->addSql('DROP TABLE _acb_page_component_group');
         $this->addSql('DROP TABLE _acb_route');
         $this->addSql('DROP TABLE blog_article_data');
+        $this->addSql('DROP TABLE html_content');
+        $this->addSql('DROP TABLE image');
+        $this->addSql('DROP TABLE navigation_link');
         $this->addSql('DROP TABLE refresh_token');
         $this->addSql('DROP TABLE "user"');
     }
