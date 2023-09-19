e31b17e8d37d971bfac1f17705cfa2c773a86b3c
silverbackdan <daniel@silverback.is>
2023-09-19 20:26:50 +0100

    Update all deps and images, use latest php and caddy - upgrade node modules

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 import { defineNuxtConfig } from 'nuxt/config'
-import {existsSync, readFileSync} from "fs";
 
 const API_URL = process.env.API_URL || 'https://localhost:8443'
 const API_URL_BROWSER = process.env.API_URL_BROWSER || API_URL
@@ -10,6 +9,7 @@ export default defineNuxtConfig({
     './node_modules/@cwa/nuxt3-next/dist/layer'
   ],
   modules: [
+    '@cwa/nuxt3-next',
     '@nuxtjs/tailwindcss',
     '@nuxt/image-edge',
     '@vite-pwa/nuxt',
--- app/tailwind.config.ts
+++ app/tailwind.config.ts
@@ -7,5 +7,8 @@ module.exports = {
   ],
   plugins: [
     require('@tailwindcss/forms')
-  ]
+  ],
+  corePlugins: {
+    preflight: false
+  }
 }
