af1b3c71126eb1969fed8825feb63646badea575
silverbackdan <daniel@silverback.is>
2023-10-23 15:40:23 +0100

    PWA Config

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -30,7 +30,48 @@ export default defineNuxtConfig({
       ]
     }
   },
-  pwa: {},
+  pwa: {
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'CWA',
+      short_name: 'CWA',
+      theme_color: '#212121',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      // installPrompt: true,
+      // you don't need to include this: only for testing purposes
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      // periodicSyncForUpdates: 20,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    }
+  },
   alias: {
     'lodash/isEqual.js': 'lodash/isEqual.js'
   }
