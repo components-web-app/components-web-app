97046cd59661cd3b3b785f07855ae2adfeba7a04
silverbackdan <daniel@silverback.is>
2023-11-14 15:05:59 +0000

    Style fix for placeholders

--- app/cwa/components/Image/Image.vue
+++ app/cwa/components/Image/Image.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <div data-placeholder class="h-40 w-40 overflow-hidden relative bg-gray-200" />
-    <nuxt-img src="/logo.svg" placeholder />
+    <div data-placeholder="true" class="h-40 w-40 overflow-hidden relative bg-gray-200" />
     <CodeBlock>{{ resource }}</CodeBlock>
   </div>
 </template>
@@ -15,19 +14,3 @@ const props = defineProps<IriProp>()
 
 const resource = useCwaResource(toRef(props, 'iri')).getResource()
 </script>
-
-<style>
-[data-placeholder]::after {
-  content: " ";
-  box-shadow: 0 0 50px 9px rgba(254,254,254);
-  position: absolute;
-  top: 0;
-  left: -100%;
-  height: 100%;
-  animation: load 1s infinite;
-}
-@keyframes load {
-  0%{ left: -100%}
-  100%{ left: 150%}
-}
-</style>
