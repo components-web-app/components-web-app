5561cc687ed90ec68c05c20ec063f5ef33c3c888
silverbackdan <daniel@silverback.is>
2024-09-04 11:00:30 +0100

    Navigation Link admin

--- app/cwa/components/NavigationLink/NavigationLink.vue
+++ app/cwa/components/NavigationLink/NavigationLink.vue
@@ -1,17 +1,31 @@
 <template>
-  <nuxt-link :to="resource?.data?.url || '#'" exact-active-class="!text-gray-900 underline" class="text-base font-medium text-gray-500 hover:text-gray-900 no-underline">
-    {{ resource?.data?.label || 'No Link Label' }}
-  </nuxt-link>
+  <div>
+    <nuxt-link :target="isExternal ? '_blank' : undefined" :to="resource?.data?.url || '#'" exact-active-class="!text-gray-900 underline" class="text-base font-medium text-gray-500 hover:text-gray-900 no-underline" @click="handleClick">
+      {{ resource?.data?.label || 'No Link Label' }}
+    </nuxt-link>
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { IriProp } from '#cwa/runtime/composables/cwa-resource'
-import { toRef } from 'vue'
+import { computed, toRef } from 'vue'
 import { useCwaResource } from '#imports'
 
 const props = defineProps<IriProp>()
 
-const { getResource, exposeMeta } = useCwaResource(toRef(props, 'iri'))
+const { getResource, exposeMeta, $cwa } = useCwaResource(toRef(props, 'iri'))
 const resource = getResource()
 defineExpose(exposeMeta)
+
+const isExternal = computed(() => {
+  return !!resource.value?.data?.rawPath
+})
+
+function handleClick (e: MouseEvent) {
+  if (!isExternal.value) {
+    return
+  }
+
+  $cwa.navigationDisabled && e.preventDefault()
+}
 </script>
--- app/cwa/components/NavigationLink/admin/LinkTab.vue
+++ app/cwa/components/NavigationLink/admin/LinkTab.vue
@@ -1,15 +1,45 @@
 <script setup lang="ts">
-import { useCwaResourceManagerTab } from '#imports'
+import { computed, ref, watch } from 'vue'
+import { useCwaResourceManagerTab, useCwaResourceModel } from '#imports'
 
-const { exposeMeta } = useCwaResourceManagerTab({
+const { exposeMeta, iri, resource } = useCwaResourceManagerTab({
   name: 'Link'
+})
+
+const showInternalRoute = ref(!resource.value?.data?.rawPath)
+const toggleLabel = computed(() => {
+  return showInternalRoute.value ? 'Internal' : 'External'
+})
+
+const routeModel = useCwaResourceModel<string>(iri, 'route', {
+  debounceTime: 0
+})
+
+const rawPathModel = useCwaResourceModel<string|null>(iri, 'rawPath')
+
+watch(showInternalRoute, (isInternal) => {
+  if (isInternal) {
+    rawPathModel.model.value = null
+  }
+})
+
+watch(() => !resource.value?.data?.rawPath, (noRawPath: boolean) => {
+  showInternalRoute.value = noRawPath
+}, {
+  immediate: true
 })
 
 defineExpose(exposeMeta)
 </script>
 
 <template>
-  <div>
-    My link tab
+  <div class="cwa-flex cwa-space-x-8">
+    <CwaUiFormToggle v-model="showInternalRoute" :label="toggleLabel" />
+    <CwaUiFormLabelWrapper v-if="showInternalRoute" label="Route">
+      <CwaUiFormSearchResource v-model="routeModel.model.value" endpoint="/_/routes" property="path" />
+    </CwaUiFormLabelWrapper>
+    <CwaUiFormLabelWrapper v-else label="URL">
+      <CwaUiFormInput v-model="rawPathModel.model.value" />
+    </CwaUiFormLabelWrapper>
   </div>
 </template>
