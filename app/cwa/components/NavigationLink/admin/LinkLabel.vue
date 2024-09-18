4a543088e88c8c12d084c424ea8bf1d36ffede9b
silverbackdan <daniel@silverback.is>
2024-09-18 19:50:19 +0200

    Update module and admin for editing link label

---
+++ app/cwa/components/NavigationLink/admin/LinkLabel.vue
@@ -0,0 +1,20 @@
+<template>
+  <CwaUiFormLabelWrapper label="Label">
+    <CwaUiFormInput v-model="labelModel.model.value" />
+  </CwaUiFormLabelWrapper>
+</template>
+
+<script setup lang="ts">
+import {
+  useCwaResourceManagerTab,
+  useCwaResourceModel
+} from '#imports'
+
+const { exposeMeta, iri } =
+  useCwaResourceManagerTab({ name: 'Label' })
+
+const labelModel =
+  useCwaResourceModel<string>(iri, 'label')
+
+defineExpose(exposeMeta)
+</script>
