7db2a47cef60756999aa585e162cb369d157f21e
silverbackdan <daniel@silverback.is>
2024-09-24 07:11:01 +0100

    Remove all containerisation and deployment - fresh for re-implementation

--- api/_docker/varnish/persist-environment.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -e
-envsubst '${UPSTREAM},${UPSTREAM_PORT},${PHP_SERVICE},${CORS_ALLOW_ORIGIN}' < /tmp/varnish/conf/default.vcl > /etc/varnish/default.vcl
-exec "$@"
--- app/_docker/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-if [ "$NODE_ENV" = 'production' ]; then
-  node .output/server/index.mjs
-else
-  pnpm install
-  pnpm run dev-ssl
-fi
