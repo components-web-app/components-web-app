89fcd6aa38cfacdba1096f88c71021db6c51f76d
silverbackdan <daniel@silverback.is>
2024-10-19 23:41:31 +0100

    Update headers defined by nitro if no cache headers set

--- app/server/plugins/nitroCacheControlPlugins.ts
+++ app/server/plugins/nitroCacheControlPlugins.ts
@@ -3,7 +3,7 @@ import {H3Event} from "h3";
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook("beforeResponse", async (event: H3Event, { body }) => {
     if (!event.headers.has('cache-control')) {
-      event.headers.set('cache-control', 'public, max-age=120, s-maxage=604800, must-revalidate')
+      event.headers.set('cache-control', 'public, max-age=7200, stale-while-revalidate=604800, s-maxage=604800')
     }
   });
 })
