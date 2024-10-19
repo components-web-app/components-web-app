e09168cac6ad9add80cbcef0bbda23b04d030756
silverbackdan <daniel@silverback.is>
2024-10-20 00:02:53 +0100

    Nitro try to use uppercase

--- app/server/plugins/nitroCacheControlPlugins.ts
+++ app/server/plugins/nitroCacheControlPlugins.ts
@@ -2,8 +2,8 @@ import {H3Event} from "h3";
 
 export default defineNitroPlugin((nitroApp) => {
   nitroApp.hooks.hook("beforeResponse", async (event: H3Event, { body }) => {
-    if (!event.headers.has('cache-control')) {
-      event.headers.set('cache-control', 'public, max-age=7200, stale-while-revalidate=604800, s-maxage=604800')
+    if (!event.headers.has('Cache-Control') || event.headers.get('Cache-Control') === '') {
+      event.headers.set('Cache-Control', 'public, max-age=7200, stale-while-revalidate=604800, s-maxage=604800')
     }
   });
 })
