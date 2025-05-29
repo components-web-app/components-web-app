02b8975ab96b294ccfbb28bd643cdf5f69c8af7c
silverbackdan <daniel@silverback.is>
2025-05-29 12:34:43 +0100

    Update api deps

---
+++ api/migrations/Version20250529113500.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250529113500 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql(<<<'SQL'
+            CREATE TABLE _acb_site_config_parameter (key VARCHAR(255) NOT NULL, value JSON DEFAULT NULL, PRIMARY KEY(key))
+        SQL);
+    }
+
+    public function down(Schema $schema): void
+    {}
+}
